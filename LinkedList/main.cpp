78e2fb2f64db264e94268603b081d68ecf8a15e0
Tech1989plus1 <31942001+Tech1989plus1@users.noreply.github.com>
2018-10-05 10:46:27 -0700

    Test Driver

---
+++ LinkedList/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  LinkedList
+//
+//  Created by Derrick Marshall on 2/7/17.
+//  Copyright © 2017 Derrick Marshall. All rights reserved.
+//
+
+#include <iostream>
+using namespace	std;
+#include "LinkedList.h"
+
+int main() {
+	ListData list;
+	
+	list.putItem(4);
+	list.putItem(5);
+	list.putItem(4);
+    return 0;
+}
